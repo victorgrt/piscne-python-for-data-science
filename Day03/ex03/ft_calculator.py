a5b3105a1df22e21dcbed806bd6a5886ddb08806
Victor GORET <vgoret@bess-f2r5s4.clusters.42paris.fr>
2025-04-05 14:15:17 +0200

    ex03 done

---
+++ Day03/ex03/ft_calculator.py
@@ -0,0 +1,28 @@
+class calculator:
+    
+    def __init__(self, vector):
+        self.vector = vector
+        
+    
+    def __add__(self, object) -> None:
+        self.vector = [number + object for number in self.vector]
+        print(self.vector)
+        return [number for number in self.vector]
+            
+    def __mul__(self, object) -> None:
+        self.vector = [number * object for number in self.vector]
+        print(self.vector)
+        return [number for number in self.vector]
+    
+    def __sub__(self, object) -> None:
+        self.vector = [number - object for number in self.vector]
+        print(self.vector)
+        return [number for number in self.vector]
+
+    def __truediv__(self, object) -> None:
+        try:
+            self.vector = [number / object for number in self.vector]
+        except:
+            return print("\033[31;1mZeroDivisionError: float division by zero\033[0m")        
+        print(self.vector)    
+        return [number for number in self.vector]
