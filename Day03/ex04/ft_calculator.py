281beaa91b3880eff486e131f614beb63d5b70d1
Victor GORET <vgoret@bess-f2r5s4.clusters.42paris.fr>
2025-04-05 14:26:21 +0200

    ex04 done

---
+++ Day03/ex04/ft_calculator.py
@@ -0,0 +1,28 @@
+class calculator:
+    
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        vector = []
+        for x in range(0, len(V1)):
+            vector.append(float(V1[x] * V2[x]))
+        res = 0
+        for num in vector:
+            res += num 
+        
+        print("Dot Product is: ", res)
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        vector = []
+        for x in range(0, len(V1)):
+            vector.append(float(V1[x] + V2[x]))
+        print("Add Vector is: ", vector)
+
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        vector = []
+        for x in range(0, len(V1)):
+            vector.append(float(V1[x] - V2[x]))
+        print("Sous Vector is:", vector)
+    
