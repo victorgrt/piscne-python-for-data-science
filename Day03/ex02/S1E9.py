c872f3617bc6e7cc6c7b451913b61da39dc0ba81
Victor GORET <vgoret@bess-f2r5s4.clusters.42paris.fr>
2025-04-05 14:00:51 +0200

    looks good but it's too easy, i feel like i'm missing something.....

--- Day03/ex02/S1E9.py
+++ Day03/ex02/S1E9.py
@@ -1,27 +1,39 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
     """
     """
     @abstractmethod
-    def __init__(self, first_name: str, is_alive=True):
+    def __init__(self, first_name: str, family_name:str , is_alive=True):
         """
         """
-        self.name = first_name
+        self.first_name = first_name
         self.is_alive = is_alive
+        self.family_name = family_name
 
     def die(self):
         """
         """
         pass
     
+    def __str__(self):
+        """"""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    
+    def __repr__(self):
+        """"""
+        return self.__str__()
+
+    
+
 class Stark(Character):
     """
     """
     def __init__(self, first_name: str, is_alive=True):
         """
         """
-        self.name = first_name
+        self.first_name = first_name
         self.is_alive = is_alive
         
         
@@ -30,15 +42,14 @@ class Stark(Character):
         """
         self.is_alive = False
         
-        
 
 def main():
     Perso = Stark("Kirikou", True)
     print(Perso)
-    print(Perso._name)
-    print(Perso._is_alive)
+    print(Perso.first_name)
+    print(Perso.is_alive)
     Perso.die()
-    print(Perso._is_alive)
+    print(Perso.is_alive)
     
 
 if __name__ == "__main__":
